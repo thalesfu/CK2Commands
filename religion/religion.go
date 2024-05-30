c3c6d805b9c467f81f551be896adf4180c3c4dec
ThalesFu <thales@thalesfu.com>
2024-05-30 15:42:46 +0800

    创建宗教的代码文件

---
+++ religion/religion.go
@@ -0,0 +1,266 @@
+package culture
+
+import "github.com/thalesfu/ck2nebula"
+
+var (
+	Religion_基督教_波格米勒派_bogomilist = &ck2nebula.Religion{
+		VID:  "culture.bogomilist",
+		Name: "波格米勒派",
+		Code: "bogomilist",
+	}
+	Religion_基督教_清洁派_cathar = &ck2nebula.Religion{
+		VID:  "culture.cathar",
+		Name: "清洁派",
+		Code: "cathar",
+	}
+	Religion_基督教_天主教_catholic = &ck2nebula.Religion{
+		VID:  "culture.catholic",
+		Name: "天主教",
+		Code: "catholic",
+	}
+	Religion_基督教_小兄弟会_fraticelli = &ck2nebula.Religion{
+		VID:  "culture.fraticelli",
+		Name: "小兄弟会",
+		Code: "fraticelli",
+	}
+	Religion_基督教_圣像破坏派_iconoclast = &ck2nebula.Religion{
+		VID:  "culture.iconoclast",
+		Name: "圣像破坏派",
+		Code: "iconoclast",
+	}
+	Religion_基督教_罗拉德派_lollard = &ck2nebula.Religion{
+		VID:  "culture.lollard",
+		Name: "罗拉德派",
+		Code: "lollard",
+	}
+	Religion_基督教_祷告派_messalian = &ck2nebula.Religion{
+		VID:  "culture.messalian",
+		Name: "祷告派",
+		Code: "messalian",
+	}
+	Religion_基督教_基督合性派_miaphysite = &ck2nebula.Religion{
+		VID:  "culture.miaphysite",
+		Name: "基督合性派",
+		Code: "miaphysite",
+	}
+	Religion_基督教_基督一性派_monophysite = &ck2nebula.Religion{
+		VID:  "culture.monophysite",
+		Name: "基督一性派",
+		Code: "monophysite",
+	}
+	Religion_基督教_基督一志派_monothelite = &ck2nebula.Religion{
+		VID:  "culture.monothelite",
+		Name: "基督一志派",
+		Code: "monothelite",
+	}
+	Religion_基督教_聂斯脱里派_nestorian = &ck2nebula.Religion{
+		VID:  "culture.nestorian",
+		Name: "聂斯脱里派",
+		Code: "nestorian",
+	}
+	Religion_基督教_东正教_orthodox = &ck2nebula.Religion{
+		VID:  "culture.orthodox",
+		Name: "东正教",
+		Code: "orthodox",
+	}
+	Religion_基督教_保禄派_paulician = &ck2nebula.Religion{
+		VID:  "culture.paulician",
+		Name: "保禄派",
+		Code: "paulician",
+	}
+	Religion_基督教_瓦勒度派_waldensian = &ck2nebula.Religion{
+		VID:  "culture.waldensian",
+		Name: "瓦勒度派",
+		Code: "waldensian",
+	}
+	Religion_东方宗教_佛教_buddhist = &ck2nebula.Religion{
+		VID:  "culture.buddhist",
+		Name: "佛教",
+		Code: "buddhist",
+	}
+	Religion_东方宗教_印度教_hindu = &ck2nebula.Religion{
+		VID:  "culture.hindu",
+		Name: "印度教",
+		Code: "hindu",
+	}
+	Religion_东方宗教_耆那教_jain = &ck2nebula.Religion{
+		VID:  "culture.jain",
+		Name: "耆那教",
+		Code: "jain",
+	}
+	Religion_东方宗教_道教_taoist = &ck2nebula.Religion{
+		VID:  "culture.taoist",
+		Name: "道教",
+		Code: "taoist",
+	}
+	Religion_犹太教_犹太教_jewish = &ck2nebula.Religion{
+		VID:  "culture.jewish",
+		Name: "犹太教",
+		Code: "jewish",
+	}
+	Religion_犹太教_圣经派_karaite = &ck2nebula.Religion{
+		VID:  "culture.karaite",
+		Name: "圣经派",
+		Code: "karaite",
+	}
+	Religion_犹太教_撒玛利亚派_samaritan = &ck2nebula.Religion{
+		VID:  "culture.samaritan",
+		Name: "撒玛利亚派",
+		Code: "samaritan",
+	}
+	Religion_伊斯兰教_德鲁兹派_druze = &ck2nebula.Religion{
+		VID:  "culture.druze",
+		Name: "德鲁兹派",
+		Code: "druze",
+	}
+	Religion_伊斯兰教_胡儒非派_hurufi = &ck2nebula.Religion{
+		VID:  "culture.hurufi",
+		Name: "胡儒非派",
+		Code: "hurufi",
+	}
+	Religion_伊斯兰教_伊巴德派_ibadi = &ck2nebula.Religion{
+		VID:  "culture.ibadi",
+		Name: "伊巴德派",
+		Code: "ibadi",
+	}
+	Religion_伊斯兰教_哈瓦利吉派_kharijite = &ck2nebula.Religion{
+		VID:  "culture.kharijite",
+		Name: "哈瓦利吉派",
+		Code: "kharijite",
+	}
+	Religion_伊斯兰教_卡尔马特派_qarmatian = &ck2nebula.Religion{
+		VID:  "culture.qarmatian",
+		Name: "卡尔马特派",
+		Code: "qarmatian",
+	}
+	Religion_伊斯兰教_什叶派_shiite = &ck2nebula.Religion{
+		VID:  "culture.shiite",
+		Name: "什叶派",
+		Code: "shiite",
+	}
+	Religion_伊斯兰教_逊尼派_sunni = &ck2nebula.Religion{
+		VID:  "culture.sunni",
+		Name: "逊尼派",
+		Code: "sunni",
+	}
+	Religion_伊斯兰教_雅兹迪派_yazidi = &ck2nebula.Religion{
+		VID:  "culture.yazidi",
+		Name: "雅兹迪派",
+		Code: "yazidi",
+	}
+	Religion_伊斯兰教_诵经派_zikri = &ck2nebula.Religion{
+		VID:  "culture.zikri",
+		Name: "诵经派",
+		Code: "zikri",
+	}
+	Religion_原始宗教_阿兹特克多神教_aztec = &ck2nebula.Religion{
+		VID:  "culture.aztec",
+		Name: "阿兹特克多神教",
+		Code: "aztec",
+	}
+	Religion_原始宗教_阿兹特克多神教_aztec_reformed = &ck2nebula.Religion{
+		VID:  "culture.aztec_reformed",
+		Name: "阿兹特克多神教",
+		Code: "aztec_reformed",
+	}
+	Religion_原始宗教_洛姆瓦教_baltic_pagan = &ck2nebula.Religion{
+		VID:  "culture.baltic_pagan",
+		Name: "洛姆瓦教",
+		Code: "baltic_pagan",
+	}
+	Religion_原始宗教_洛姆瓦教_baltic_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.baltic_pagan_reformed",
+		Name: "洛姆瓦教",
+		Code: "baltic_pagan_reformed",
+	}
+	Religion_原始宗教_苯教_bon = &ck2nebula.Religion{
+		VID:  "culture.bon",
+		Name: "苯教",
+		Code: "bon",
+	}
+	Religion_原始宗教_苯教_bon_reformed = &ck2nebula.Religion{
+		VID:  "culture.bon_reformed",
+		Name: "苯教",
+		Code: "bon_reformed",
+	}
+	Religion_原始宗教_索米多神教_finnish_pagan = &ck2nebula.Religion{
+		VID:  "culture.finnish_pagan",
+		Name: "索米多神教",
+		Code: "finnish_pagan",
+	}
+	Religion_原始宗教_索米多神教_finnish_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.finnish_pagan_reformed",
+		Name: "索米多神教",
+		Code: "finnish_pagan_reformed",
+	}
+	Religion_原始宗教_希腊多神教_hellenic_pagan = &ck2nebula.Religion{
+		VID:  "culture.hellenic_pagan",
+		Name: "希腊多神教",
+		Code: "hellenic_pagan",
+	}
+	Religion_原始宗教_希腊多神教_hellenic_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.hellenic_pagan_reformed",
+		Name: "希腊多神教",
+		Code: "hellenic_pagan_reformed",
+	}
+	Religion_原始宗教_日耳曼多神教_norse_pagan = &ck2nebula.Religion{
+		VID:  "culture.norse_pagan",
+		Name: "日耳曼多神教",
+		Code: "norse_pagan",
+	}
+	Religion_原始宗教_日耳曼多神教_norse_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.norse_pagan_reformed",
+		Name: "日耳曼多神教",
+		Code: "norse_pagan_reformed",
+	}
+	Religion_原始宗教_斯拉夫多神教_slavic_pagan = &ck2nebula.Religion{
+		VID:  "culture.slavic_pagan",
+		Name: "斯拉夫多神教",
+		Code: "slavic_pagan",
+	}
+	Religion_原始宗教_斯拉夫多神教_slavic_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.slavic_pagan_reformed",
+		Name: "斯拉夫多神教",
+		Code: "slavic_pagan_reformed",
+	}
+	Religion_原始宗教_非洲泛灵教_west_african_pagan = &ck2nebula.Religion{
+		VID:  "culture.west_african_pagan",
+		Name: "非洲泛灵教",
+		Code: "west_african_pagan",
+	}
+	Religion_原始宗教_非洲泛灵教_west_african_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.west_african_pagan_reformed",
+		Name: "非洲泛灵教",
+		Code: "west_african_pagan_reformed",
+	}
+	Religion_原始宗教_尊日神教_zun_pagan = &ck2nebula.Religion{
+		VID:  "culture.zun_pagan",
+		Name: "尊日神教",
+		Code: "zun_pagan",
+	}
+	Religion_原始宗教_尊日神教_zun_pagan_reformed = &ck2nebula.Religion{
+		VID:  "culture.zun_pagan_reformed",
+		Name: "尊日神教",
+		Code: "zun_pagan_reformed",
+	}
+	Religion_马兹达教_胡天派_khurmazta = &ck2nebula.Religion{
+		VID:  "culture.khurmazta",
+		Name: "胡天派",
+		Code: "khurmazta",
+	}
+	Religion_马兹达教_摩尼教_manichean = &ck2nebula.Religion{
+		VID:  "culture.manichean",
+		Name: "摩尼教",
+		Code: "manichean",
+	}
+	Religion_马兹达教_马兹达克派_mazdaki = &ck2nebula.Religion{
+		VID:  "culture.mazdaki",
+		Name: "马兹达克派",
+		Code: "mazdaki",
+	}
+	Religion_马兹达教_祆教_zoroastrian = &ck2nebula.Religion{
+		VID:  "culture.zoroastrian",
+		Name: "祆教",
+		Code: "zoroastrian",
+	}
+)
